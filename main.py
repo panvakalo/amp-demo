7a6ed59fce8f147eaf9e4956b3194f0e02c9c8ca
Gilad Maoz <gilad.maoz@gmail.com>
2019-05-24 15:20:30 +0200

    - added appengine yaml conf - cleaned the req

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from flask import Flask, render_template, request
 
 app = Flask('VidaXL landingPage', static_url_path='/static')
 
+
 @app.route("/")
 def main():
     query = request.args.get('query')
-    response = requests.get('http://10.51.62.67:28321/api/v1/stores/nl_nl/pages/product-listings?search_text={}'.format(query))
+    response = requests.get(
+        'http://10.51.62.67:28321/api/v1/stores/nl_nl/pages/product-listings?search_text={}'.format(query))
 
     return render_template('main.html', items=response.json().get('product_list_items') if response else {})
 
